080db5673b6f0795ec56da9ad7d4d12da6ead458
Collyn Philleo <collyn.philleo@automattic.com>
2020-04-23 14:50:26 -0700

    Add configuration settings for California Consumer Privacy Act (CCPA)

--- client/my-sites/marketing/traffic/index.js
+++ client/my-sites/marketing/traffic/index.js
@@ -43,6 +43,7 @@ const SiteSettingsTraffic = ( {
 	isJetpackAdmin,
 	isRequestingSettings,
 	isSavingSettings,
+	onChangeField,
 	setFieldValue,
 	translate,
 } ) => (
@@ -64,6 +65,8 @@ const SiteSettingsTraffic = ( {
 				isSavingSettings={ isSavingSettings }
 				isRequestingSettings={ isRequestingSettings }
 				fields={ fields }
+				onSubmitForm={ handleSubmitForm }
+				onChangeField={ onChangeField }
 			/>
 		) }
 		{ isAdmin && <SeoSettingsHelpCard disabled={ isRequestingSettings || isSavingSettings } /> }
@@ -128,6 +131,8 @@ const getFormSettings = partialRight( pick, [
 	'count_roles',
 	'roles',
 	'enable_header_ad',
+	'wordads_ccpa_enabled',
+	'wordads_ccpa_privacy_policy_url',
 	'jetpack_relatedposts_allowed',
 	'jetpack_relatedposts_enabled',
 	'jetpack_relatedposts_show_headline',
